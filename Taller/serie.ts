f3ce417418351480259a4c10f02e9435b612f539
ManuelaLizcano <111076043+ManuelaLizcano@users.noreply.github.com>
2023-10-22 16:09:55 -0500

    Add files via upload

---
+++ Taller/serie.ts
@@ -0,0 +1,5 @@
+export class Serie{
+    constructor(public numero: number, public nombre: string, public plataforma:string, public temporadas: number, public sinopsis: string, public link:string, public foto: string){
+
+    }
+}
